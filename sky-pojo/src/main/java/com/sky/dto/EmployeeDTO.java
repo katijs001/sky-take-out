7f17fd844ac9d6011abd697d767db0e2ad440186
卡利 <2134751035@qq.com>
2025-12-27 19:06:25 +0800

    添加mybatisPlus

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,12 +1,21 @@
 package com.sky.dto;
 
+import ch.qos.logback.classic.db.names.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("employee")
 public class EmployeeDTO implements Serializable {
 
+    @TableId(type = IdType.AUTO)
     private Long id;
 
     private String username;
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,25 +1,23 @@
 package com.sky.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.sky.entity.Employee;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
-public interface EmployeeMapper {
+public interface EmployeeMapper extends BaseMapper<Employee> {
 
     /**
      * 根据用户名查询员工
+     * 这个方法可以保留，因为使用了自定义查询条件
+     * 或者可以使用 MyBatis Plus 的 LambdaQueryWrapper 在 Service 层实现
      * @param username
      * @return
      */
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
-    /*插入员工数据
-    **/
-    @Insert("insert into emplogee(name,user,password,phone,sex,id_number,creat_time,update_time,creat_user,update_user)" +
-            "values "+
-            "#(name),#(user),")
-    void insert(Employee employee);
+    // insert 方法已经不需要了，可以直接使用 BaseMapper 的 insert 方法
+    // void insert(Employee employee);
 }
